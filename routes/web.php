c621bc4eaee74bcb8b81b312bef155f234620aad
infern <semenmochalov55@gmail.com>
2024-04-12 23:21:34 +0700

    Add admin panel template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
