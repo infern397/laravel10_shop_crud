101eba19df641ecfafd8e51a12b2ab86ebb826f6
infern <semenmochalov55@gmail.com>
2024-04-19 13:21:53 +0700

    Change stock of product in controller

--- app/Http/Controllers/Admin/OrderProductController.php
+++ app/Http/Controllers/Admin/OrderProductController.php
@@ -15,19 +15,28 @@ class OrderProductController extends Controller
     public function store(AddProductRequest $request, Order $order)
     {
         $order->products()->attach($request['product'], ['quantity' => $request['quantity']]);
+        $product = Product::query()->find($request['product']);
+        $product->stock -= $request['quantity'];
+        $product->save();
         return Redirect::back();
     }
 
     public function destroy(Order $order, Product $product)
     {
+        $quantity = $order->products()->withPivot('quantity')->find($product->id)->pivot->quantity;
         $order->products()->detach($product->id);
+        $product->stock += $quantity;
+        $product->save();
         return Redirect::back();
 
     }
 
     public function update(UpdateProductRequest $request, Order $order, Product $product)
     {
+        $prevQuantity = $order->products()->withPivot('quantity')->find($product->id)->pivot->quantity;
         $order->products()->syncWithoutDetaching([$product->id => ['quantity' => $request['quantity']]]);
+        $product->stock += $prevQuantity - $request['quantity'];
+        $product->save();
         return Redirect::back();
     }
 }
