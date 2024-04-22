075927cc3373256eca9be75a0da9dd8d715c001b
infern <semenmochalov55@gmail.com>
2024-04-22 12:26:54 +0700

    Separate web groups by files

--- routes/web.php
+++ routes/web.php
@@ -8,31 +8,3 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Client\CartController;
 use App\Http\Controllers\Client\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('admin')->group(function () {
-    Route::name('admin.')->group(function () {
-        Route::get('/', function () {
-            return view('admin.index');
-        })->name('index');
-        Route::resource('categories', CategoryController::class);
-        Route::resource('products', ProductController::class);
-        Route::resource('orders', OrderController::class);
-        Route::resource('orders.products', OrderProductController::class)->only([
-            'store', 'destroy', 'update'
-        ]);
-        Route::resource('users', UserController::class);
-    });
-});
-
-Route::name('client.')->group(function () {
-    Route::get('/', function () {
-        return view('client.welcome');
-    })->name('welcome');
-    Route::get('/products', [MainController::class, 'index'])->name('index');
-    Route::prefix('cart')->name('cart.')->controller(CartController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::delete('/{product}', 'destroy')->name('destroy');
-        Route::patch('/{product}', 'update')->name('update');
-    });
-});
